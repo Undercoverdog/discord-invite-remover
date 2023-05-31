09b4f516cd864287e642a9f6420bf0f724429b96
Dave <daveistdave@gmail.com>
2023-05-31 02:02:46 +0200

    v0.1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
+
 import json
+from datetime import timedelta
+
 
 
 ###### JSON ###########
@@ -8,15 +11,45 @@ import json
 with open("data.json", "r") as f:
     data = json.load(f)
     token = data["token"]
-
+    Roles = data["roles"]
 
 
 ######## DISCORD BOT ##############
 # Idk what this does, but it only worked when I copy pasted it
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+
 
 client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}#{client.user.discriminator}')
+
+# Invite check
+@client.event
+async def on_invite_create(invite):
+    global Roles
+    if invite.max_age == 0:
+        # Check if user is mod
+        member = invite.guild.get_member(invite.inviter.id)
+        for role in member.roles:
+            if role.id in Roles:
+               #print("Member is mod\n")
+                return # Return if member is mod/admin/etc
+
+        # Deletes invite
+        await invite.delete()
+        print(f"Deleted permanent invite: {invite.code}")
+
+        # DM user
+        new_invite = await invite.channel.create_invite(max_age=7*24*60*60) # Create 7 day invite
+        new_expires_at = new_invite.created_at + timedelta(seconds=invite.max_age) # Time the invite expires
+        await invite.inviter.send(f"Your invite has been deleted since we do not allow permanent invites. Instead use the following code. It expires at `{new_expires_at}\`.")
+        await invite.inviter.send(f"<{new_invite.url}>")
+
 
 
 
